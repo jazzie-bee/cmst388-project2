a4763393b0641e6170bfcd3c85ef823dbfa65bc7
Jazzie-bee <98238715+jazzie-bee@users.noreply.github.com>
2026-09-04 22:18:26 -0500

    change background color and moved preventDefault

--- js/project2.js
+++ js/project2.js
@@ -1,14 +1,14 @@
-const fname = document.getElementById('fname')
-const lname = document.getElementById('lname')
-const address = document.getElementById('address')
-const city = document.getElementById('city')
-const state = document.getElementById('state')
-const zipcode = document.getElementById('zipcode')
-const telarea = document.getElementById('telarea')
-const telnum = document.getElementById('telnum')
-const email = document.getElementById('email')
-const cemail = document.getElementById('cemail')
-const comment = document.getElementById('comment')
+// const fname = document.getElementById('fname')
+// const lname = document.getElementById('lname')
+// const address = document.getElementById('address')
+// const city = document.getElementById('city')
+// const state = document.getElementById('state')
+// const zipcode = document.getElementById('zipcode')
+// const telarea = document.getElementById('telarea')
+// const telnum = document.getElementById('telnum')
+// const email = document.getElementById('email')
+// const cemail = document.getElementById('cemail')
+// const comment = document.getElementById('comment')
 const form = document.getElementById('form')
 const errorElement = document.getElementsByClassName('error-message')
 
@@ -19,21 +19,25 @@ const errorElement = document.getElementsByClassName('error-message')
 // }
 
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let isValid = true;
+
     let messages = []
     if (fname.value === '' || fname.value == null) {
-        messages.push('This field is required')
+        messages.push('This field is required');
     }
 
     if (lname.value === '' || lname.value == null) {
-        messages.push('This field is required')
+        messages.push('This field is required');
     }
 
     if (address.value === '' || address.value == null) {
-        messages.push('This field is required')
+        messages.push('This field is required');
     }
     
     if (city.value === '' || city.value == null) {
-        messages.push('This field is required')
+        messages.push('This field is required');
     }
     
     if (state.value === '' || state.value == null) {
@@ -43,26 +47,21 @@ form.addEventListener('submit', (e) => {
     // if (zipcode.length >= 5) 'Zip code must be 5 digits'; numerci only
 
     if (telarea.value === '' || phone.value == null) {
-        messages.push('This field if required')
+        messages.push('This field if required');
     }
 
     if (telnum.value === '' || phone.value == null) {
-        messages.push('This field if required')
+        messages.push('This field if required');
     }
 
     // if (phone.length )
 
     if (email.value === '' || email.value == null) {
-        messages.push('This field is required')
+        messages.push('This field is required');
     }
     // Checks for correct format 'Please enter valid email address (name@domain.com)'
     
     if (cemail.value === '' || cemail.value == null) {
-        messages.push('Emails must match')
-    }
-
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText =  messages.join(', ')
+        messages.push('Emails must match');
     }
 })
